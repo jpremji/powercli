cc9fe599288e1b8417da2f0fda610f77b3225b69
jpremji <42052590+jpremji@users.noreply.github.com>
2020-08-02 21:41:12 -0400

    Update auto1904.ps1

--- labnet/auto1904.ps1
+++ labnet/auto1904.ps1
@@ -13,11 +13,12 @@
 $template = Get-Template -Name "Win10_1904"
 $custspec = "WIN10_1904_PowerCLI"
 $vlanid=223
+$vlanMax=233
 $vmhost = Get-VMHost -Name "10.80.254.21"
 $mycluster = Get-Cluster -Name "vSAN-LABNET-00"
 #$vlanid | foreach { -- Need to work on automating entire vlan range //commented out for the time being and ran one at a time
 
-
+while ($vlanid -lt $vlanMax) {
 $newvm = ("Win10_$vlanid")
 $custspec = "WIN10_1904_PowerCLI"
 $portgroup = "vDS-PG-$vlanid"
@@ -31,6 +32,9 @@ New-VM -Name $newvm -Template $template -ResourcePool $mycluster -OSCustomizatio
 
 try {Get-VM $newvm | Get-NetworkAdapter | Set-NetworkAdapter -Portgroup $portgroup -Confirm:$false -ErrorAction Stop}
 catch {break}
+Start-Sleep 30
+$vlanid++
+}
 #}
 ####Customization of the guest os failed for some settings --> New-VM : 8/2/2020 1:04:53 PM    New-VM          The operation for the entity "Win10_1904" failed with the following message: "Customization of the guest
 #operating system 'windows9_64Guest' is not supported in this configuration. Microsoft Vista (TM) and Linux guests with Logical Volume Manager are
